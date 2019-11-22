b2a3bea417d488a2e14266bda6ad64cfa1ce80a0
Jorge Alberto <51796956+JAlberto-Coder@users.noreply.github.com>
2019-11-22 10:49:39 -0600

    Scripts para bases de datos manejadas en MySQL

---
+++ DQL/SELECT_ConREGEXP.sql
@@ -0,0 +1,25 @@
+-- =============================================
+-- Version: 1.0.0.0
+-- Archivo: SELECT_ConREGEXP.sql
+-- =============================================
+-- Autor: JAlberto-Coder
+-- Fecha: 04-05-2019
+-- Descripcion: Consultas aplicando REGEXP, esto nos srive para poder encontrar campos que puedes ser validos o no en base a una expresion regular, funcion especial para realizar una depuracion de datos
+-- =============================================
+-- NOTAS:
+-- La expresiones a aplicar puedes ser tanto REGEXP o NOT REGEXP, dependiendo de lo que se desee buscar
+-- Seleccionar todos los paises que no contienen números solo letras del abecedario y en minisculas
+-- =============================================
+SELECT *
+FROM cat_paises
+WHERE pais REGEXP '[a-z]';
+
+-- Seleccionar todos los paises que comienzas con M
+SELECT *
+FROM cat_paises
+WHERE pais REGEXP '^M';
+
+-- Seleccionar todos los paises que terminan con co
+SELECT * 
+FROM cat_paises
+WHERE pais REGEXP 'co$';
---
+++ README.md
@@ -0,0 +1,11 @@
+# MySQL
+Scripts para las bases de datos manejadas por MySQL
+
+**Premisas:**
+* Cada archivo contiene su propia descripción de lo que hace.
+
+**Repositorio:**
+* **DML**: 
+  * Contiene sentencias para la manipulación de datos dentro del gestos de base de datos MySQL
+
+_A veces el mejor método para hacer algo es lanzarse. (Motoko Kusanagi)_
